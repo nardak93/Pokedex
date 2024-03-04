1cbd6e95ebf34b74aaa0c946e861110c955cb7e3
nardak93 <nardak1993@gmail.com>
2024-03-04 09:52:06 +0100

    Pokedex

---
+++ pokedex/src/App.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+import './App.css'
+import PresentationPokedex from './components/PresentationPokedex'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PresentationPokedex />
+      </header>
+    </div>
+  
+  )
+}
+
+export default App
---
+++ pokedex/src/components/PresentationPokedex.jsx
@@ -0,0 +1,18 @@
+
+
+function PresentationPokedex() {
+    return (
+        <div>
+            <h1>Pokedex</h1>
+            <p>Découvrez le monde fascinant des Pokémon avec notre Pokédex ultime! Que vous
+soyez un Maître Pokémon en devenir ou simplement curieux de découvrir chaque
+créature unique, notre base de données complète est votre porte d&apos;entrée vers
+une aventure extraordinaire. Explorez les caractéristiques, les forces, et les
+faiblesses de chaque Pokémon, plongez dans leur univers, et devenez l&apos;expert
+ultime</p>
+            
+        </div>
+    );
+}
+
+export default PresentationPokedex;
